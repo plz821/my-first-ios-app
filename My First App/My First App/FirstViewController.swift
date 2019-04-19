9a30794f90615f88cdc458cbbf796572cfe1286b
cole pan <plz821@163.com>
2019-04-19 20:21:41 +0800

    initial commit

---
+++ My First App/My First App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  My First App
+//
+//  Created by 潘凌震 on 2019/4/19.
+//  Copyright © 2019 plz821. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ My First App/My First App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  My First App
+//
+//  Created by 潘凌震 on 2019/4/19.
+//  Copyright © 2019 plz821. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
